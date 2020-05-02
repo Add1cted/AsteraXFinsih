94c8c9ceda2318aaaa374107898b14233f83102f
Mitchell Melrose <wmm18a@acu.edu>
2020-05-02 00:56:27 -0500

    final commit

---
+++ Assets/__Scripts/MobileFireButton.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileFireButton : MonoBehaviour
+{
+	//hi
+	bool registeredWithPauseChanged = false;
+
+	Image img;
+
+	void Start ()
+	{
+			img = GetComponent<Image>();
+			img.raycastTarget = false;
+
+			if(Application.isMobilePlatform)
+			{
+				RegisterWithPauseChanged();
+				PauseChangedCallback();
+			}
+			else
+			{
+				#if UNITY_EDITOR
+					StartCoroutine(CheckForUnityRemote(1));
+				#else
+					gameObject.SetActive(false);
+				#endif
+			}
+	}
+
+	IEnumerator CheckForUnityRemote(float delay)
+	{
+		while(!registeredWithPauseChanged){
+			if(UnityEditor.EditorApplication.isRemoteConnected)
+			{
+				RegisterWithPauseChanged();
+				PauseChangedCallback();
+			}
+			else
+			{
+				yield return new WaitForSeconds(delay);
+			}
+		}
+	}
+
+	void RegisterWithPauseChanged()
+	{
+			if(registeredWithPauseChanged) return;
+
+			AsteraX.PAUSED_CHANGE_DELEGATE -= PauseChangedCallback;
+			AsteraX.PAUSED_CHANGE_DELEGATE += PauseChangedCallback;
+
+			registeredWithPauseChanged = true;
+
+	}
+
+	void PauseChangedCallback()
+	{
+		img.raycastTarget = !AsteraX.PAUSED;
+	}
+}
